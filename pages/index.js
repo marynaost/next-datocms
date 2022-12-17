08ec96ccdae95236363ac7f63487c99dc8dbc84a
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-18 00:56:35 +0200

    added  cards without locale

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,56 @@
-// import { gql, GraphQLClient } from 'graphql-request';
+import { gql, GraphQLClient } from 'graphql-request';
+import InfoSection from 'components/InfoSection/InfoSection';
 // import { useRouter } from 'next/router';
 
-// const Home = ({ allBannerHeaders }) => {
-const Home = () => {
+const query = gql`
+  query {
+    allInfos {
+      data {
+        title
+        id
+        cardInfo {
+          title
+          description
+          buttonName
+          id
+          slug
+          bgImage {
+            alt
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getStaticProps = async () => {
+  const graphQLClient = new GraphQLClient(process.env.DATOCMS_API_URL, {
+    headers: {
+      'content-type': 'application/json',
+      authorization: 'Bearer ' + process.env.DATOCMS_API_KEY,
+    },
+  });
+
+  const data = await graphQLClient.request(query);
+
+  return {
+    props: {
+      cardInfo: data.allInfos,
+    },
+  };
+};
+const Home = ({ cardInfo }) => {
   // const { locale } = useRouter();
   // const bannerData = allBannerHeaders
   //   .flatMap(el => el._allBannerDescriptionLocales)
   //   .filter(el => el.locale === locale)
   //   .flatMap(el => el.value);
+  // console.log(volunteer);
+
+  // bannerData.map(el => {
+
+  // });
 
   return (
     <section className="py-20">
@@ -17,43 +60,21 @@ const Home = () => {
         </h1>
 
         {/* <div>
-          {bannerData.map(el => {
-            return el.displayBanner && <h2 key={el.id}>{el.description}</h2>;
+          {data.map(el => {
+            return (
+              <h2
+                key={el.id}
+                className="bg-stone-900 p-2 text-center text-gray-200 "
+              >
+                {el.bannerText}
+              </h2>
+            );
           })}
         </div> */}
+        <InfoSection info={cardInfo} />
       </div>
     </section>
   );
 };
 
 export default Home;
-
-// const query = gql`
-//   query {
-//     allBannerHeaders {
-//       _allBannerDescriptionLocales {
-//         locale
-//         value {
-//           id
-//           displayBanner
-//           description
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// export const getStaticProps = async () => {
-//   const graphQLClient = new GraphQLClient(process.env.DATOCMS_API_URL, {
-//     headers: {
-//       'content-type': 'application/json',
-//       authorization: 'Bearer ' + process.env.DATOCMS_API_KEY,
-//     },
-//   });
-
-//   const data = await graphQLClient.request(query);
-
-// return {
-//   props: data,
-// };
-// };
