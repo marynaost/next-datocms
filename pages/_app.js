58f1e047505fb3acc8fff010230de3ac237f7ad9
Maryna Ostapliuk <marynaum01@gmail.com>
2022-12-17 14:27:52 +0200

    init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { Layout } from 'layout';
+import { appWithTranslation } from 'next-i18next';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default appWithTranslation(MyApp);
